45339d569e4675ffd1c9468053d5a842408f31c3
mitochon9 <mitochon9@gmail.com>
2022-02-12 14:27:39 +0900

    learn down cast up cast non null assertion double assertion

--- index.ts
+++ index.ts
@@ -1,15 +1,36 @@
-export const Obj = {
-  foo: "foo",
-  111: "bar",
-};
+// ダウンキャスト
 
-// type Obj = typeof Obj;
-// type Key = keyof Obj;
+// export const color = "red";
 
-type Key = keyof typeof Obj;
+// const theme = {
+//   color: "red",
+//   backgroundColor: "blue",
+// } as const;
 
-function test(x: keyof typeof Obj) {
-  return;
+// theme.color = "red";
+
+// export const color = "red" as const;
+// let x = color;
+
+// function foo() {
+//   return { foo: "foo" } as const;
+// }
+// const y = foo();
+
+//
+
+// アップキャスト（型の抽象度を上げるものなのであまり使うべきではない）
+
+// Non null assertion（危険なコード、余り使うべきではない）
+// export function getFirstLetter(str?: string) {
+//   // 対処法はundefinedのときにreturnさせる
+//   if (str!) {
+//     return;
+//   }
+//   return str?.charAt(0);
+// }
+
+//double assertion（あまり使うべきではない）
+export function getFirstLetter(str: number) {
+  return (str as unknown as string).charAt(0);
 }
-
-test("foo");
